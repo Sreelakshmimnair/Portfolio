d286265e0fafd2c3d2dfb26740983382dc8cd2f5
mohdnishal <nishalutd@gmail.com>
2024-07-18 00:40:01 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import '../App.css';
+import Profile from '../assets/profile.jpg';
+import Resumes from '../assets/Resume.pdf';
+
+const About = () => {
+  return (
+    <>
+      <div className="about-section" id='home'>
+        <Container className="h-100">
+          <Row className="h-100 align-items-center">
+            <Col md={6} className="about-content">
+              <p className='fs-5 font-weight-bold title-para'>HELLO!</p>
+              <h1 className='name-title'>I'm <strong>Sreelakshmi <br/> M Nair</strong></h1>
+              <p className="name-title-desc">A Computer Engineer</p>
+            </Col>
+            <Col md={6} className="text-center">
+              <Image src={Profile} className="profile-image" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="about-section" id="about">
+        <Container className="h-100">
+          <Row>
+            <Col>
+              <h1 className='text-center about-title'>About Me</h1>
+              <p className='about-text'>
+                Hello! I'm Sreelakshmi M Nair, a passionate software developer. I specialize in creating dynamic and beautiful web pages. A motivated and dedicated final-year Computer Science and Engineering student seeking an opportunity to apply technical skills and contribute to a dynamic team. Passionate about software development and problem-solving.
+              </p>
+              <div className="text-center">
+                <a className="download-cv-button" style={{textDecoration:'none'}} href={Resumes} download>DOWNLOAD CV</a>
+                {/* <button className="download-cv-button" href="/path/to/your/cv.pdf" download></button> */}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Resume.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import '../App.css';
+import Resumes from '../assets/Resume.pdf';
+
+const Resume = () => {
+  const educationDetails = [
+    {
+      school: 'Rajiv Gandhi Institute of Technology, Kottayam',
+      degree: 'B.Tech in Computer Science & Engineering',
+      duration: '2021 - 2025',
+    },
+   
+    {
+      school: 'MDS HSS KOTTAYAM',
+      degree: 'HIGHER Secondary Education',
+      duration: '2019 - 2021',
+    },
+    {
+      school: "St.MARY'S GHS ATHIRAMPUZHA",
+      degree: 'Secondary Education',
+      duration: '2018 - 2019',
+    }
+   
+  ];
+
+  return (
+    <div className="resume-section">
+      <Container className="h-100"  id="resume">
+        <Row className="h-100 align-items-center justify-content-center">
+          <Col md={10}>
+            <h1 className="text-center resume-title">Resume</h1>
+            <Row>
+              {educationDetails.map((edu, index) => (
+                <Col md={6} key={index} className="mb-4">
+                  <Card className="education-card bg-dark">
+                    <Card.Body>
+                      <Card.Title className="text-white">{edu.school}</Card.Title>
+                      <Card.Subtitle className="mb-2" style={{color:'#999999'}}>{edu.degree}</Card.Subtitle>
+                      <Card.Text className="text-warning">
+                        <strong>{edu.duration}</strong>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+              <div className="text-center w-100 mt-4">
+              <a className="download-cv-button" style={{textDecoration:'none'}} href={Resumes} download>DOWNLOAD CV</a>
+                {/* <button className="download-cv-button" href="/path/to/your/cv.pdf" download>DOWNLOAD CV</button> */}
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Resume;
